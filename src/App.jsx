6698d1f7cb9979a62754fd7b37a404d8ebf7a6b4
SandraCarretero <sandracarretero24@gmail.com>
2024-02-29 09:53:26 +0100

    change card

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import Card from './components/card/Card';
 import Container from './components/container/Container';
 
 const App = () => {
 	return (
 		<>
-			<Container>
-			<Card
-                    icon="/images/icon-sedans.svg"
-                    title="Sedans"
-                    text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
-                    color="#e28625"
-                />
-                <Card
-                    icon="/images/icon-suvs.svg"
-                    title="Suvs"
-                    text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-                    color="#006971"
-                />
-                <Card
-                    icon="/images/icon-luxury.svg"
-                    title="Luxury"
-                    text="Cruise in the best bar brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
-                    color="#004140"
-                />
-			</Container>
+			<Container />
 		</>
 	);
 };
--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,7 +1,27 @@
+import Card from "../card/Card";
 import { StyledContainer } from "./container.styles";
 
-const Container = ({ children }) => (
-	<StyledContainer>{children}</StyledContainer>
+const Container = () => (
+	<StyledContainer>
+		<Card
+                    icon="/images/icon-sedans.svg"
+                    title="Sedans"
+                    text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+                    color="#e28625"
+                />
+                <Card
+                    icon="/images/icon-suvs.svg"
+                    title="Suvs"
+                    text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+                    color="#006971"
+                />
+                <Card
+                    icon="/images/icon-luxury.svg"
+                    title="Luxury"
+                    text="Cruise in the best bar brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+                    color="#004140"
+                />
+	</StyledContainer>
     
 );
 
